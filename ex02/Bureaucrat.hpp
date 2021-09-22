670269ad0cde83abed5b3323ab39df0bf3e75893
grvelva <grvelva@student.21-school.ru>
2021-09-22 18:03:42 +0300

    начал екз02

---
+++ ex02/Bureaucrat.hpp
@@ -0,0 +1,61 @@
+/**
+At least:
+- default constructor
+- copy constructor
+- assignation operator overload
+- destructor.
+ */
+
+#ifndef Bureaucrat_HPP
+#define Bureaucrat_HPP
+
+#include <string>
+#include <iostream>
+#include "Form.hpp"
+
+#define CREATE_MSG "Try to create Bureaucrat with grade "
+#define INC_MSG "Try to inc Bureaucrat with grade "
+#define DEC_MSG "Try to dec Bureaucrat with grade "
+#define SIGN_MSG "Try to sign form"
+#define BURO_NAME "Dima"
+#define BEGIN 	"|========================|\n"
+#define END 	"|________________________|\n\n"
+
+class Form;
+
+class Bureaucrat
+{
+protected:
+	const std::string	_name;
+	int					_grade;
+	Bureaucrat();
+	
+public:
+	const std::string &getName() const;
+	const int &getGrade() const;
+	void incGrade();
+	void decGrade();
+	
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(Bureaucrat &other);
+	Bureaucrat & operator=(Bureaucrat const &other);
+	~Bureaucrat();
+	
+	void signForm(Form &form);
+	
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		const char* what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		const char* what() const throw();
+	};
+};
+
+std::ostream& operator<<(std::ostream &stream, const Bureaucrat &buddy);
+
+#endif //Bureaucrat_HPP
---
+++ ex02/Form.hpp
@@ -0,0 +1,58 @@
+/**
+At least:
+- default constructor
+- copy constructor
+- assignation operator overload
+- destructor.
+ */
+
+#ifndef Form_HPP
+#define Form_HPP
+
+#include <string>
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+#define F_CREATE_MSG "Try to create Form with grade "
+
+class Bureaucrat;
+
+class Form
+{
+private:
+	const std::string	_name;
+	const int			_gradeToSign;
+	const int			_gradeToExec;
+	bool 				_isSigned;
+	Form();
+	
+public:
+	const std::string &getName() const;
+	int getGradeToSign() const;
+	int getGradeToExec() const;
+	bool isSigned() const;
+	
+	Form(const std::string &name, int gradeToSign = 0,
+		 int gradeToExec = 0);
+	Form(Form &other);
+	Form & operator=(Form const &other);
+	~Form();
+	
+	void beSigned(Bureaucrat const &buddy);
+	
+	class GradeTooHighException : public std::exception
+		{
+		public:
+			const char* what() const throw();
+		};
+
+	class GradeTooLowException : public std::exception
+		{
+		public:
+			const char* what() const throw();
+		};
+};
+
+std::ostream& operator<<(std::ostream &stream, const Form &form);
+
+#endif //Form_HPP
